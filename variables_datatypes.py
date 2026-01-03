5939ac4428e112c6c0086f24d16426c269695f54
Ayesha Saleem <ayesha.ai.deve@gmail.com>
2026-01-03 19:17:44 +0500

    Day 02: Variables and Data Types

---
+++ variables_datatypes.py
@@ -0,0 +1,52 @@
+# Variables & Data Types
+# Author: Ayesha Saleem
+
+# -----------------------------
+# Variables
+# -----------------------------
+
+student_name = "Ayesha Saleem"
+student_age = 20
+student_cgpa = 3.8
+is_student = True
+
+# -----------------------------
+# Printing Variables
+# -----------------------------
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Student CGPA:", student_cgpa)
+print("Is Student:", is_student)
+
+print("-" * 30)
+
+# -----------------------------
+# Data Types
+# -----------------------------
+
+print("Data Types:")
+print("student_name type:", type(student_name))
+print("student_age type:", type(student_age))
+print("student_cgpa type:", type(student_cgpa))
+print("is_student type:", type(is_student))
+
+print("-" * 30)
+
+# -----------------------------
+# Variable Reassignment
+# -----------------------------
+
+student_age = 21
+print("Updated Age:", student_age)
+
+# -----------------------------
+# Multiple Assignment
+# -----------------------------
+
+x, y, z = 10, 20, 30
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+print("-" * 30)
